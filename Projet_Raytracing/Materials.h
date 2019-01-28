2b82575050a0756cdfb1b40db830f41837931354
TheWindWalker <arnaud.borio@orange.fr>
2019-01-28 19:05:22 +0100

    Add lightsource and Materials

--- Projet_Raytracing/Materials.h
+++ Projet_Raytracing/Materials.h
@@ -2,7 +2,10 @@
 class Materials
 {
 public:
+	int red, green, blue, reflection;
 	Materials();
+	Materials(int material_red, int material_green, int material_blue, int material_reflection);
 	~Materials();
+	
 };
 
